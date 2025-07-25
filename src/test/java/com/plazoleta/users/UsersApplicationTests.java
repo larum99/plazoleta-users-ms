ea7ffad346553c3941e87d344ca926a81c3ed858
Laura <laura.barona@uao.edu.co>
2025-07-25 06:12:10 -0500

    adjust test config and add application-test.yml

--- src/test/java/com/plazoleta/users/UsersApplicationTests.java
+++ src/test/java/com/plazoleta/users/UsersApplicationTests.java
@@ -3,7 +3,7 @@ package com.plazoleta.users;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.profiles.active=test")
 class UsersApplicationTests {
 
 	@Test
